36c1af314e399f65972a960148de2009a35f180a
Errol W <ewatsonrei@gmail.com>
2019-01-23 06:47:36 +0000

    Done.

--- lib/a_name_error.rb
+++ lib/a_name_error.rb
@@ -1,3 +1,3 @@
 "hello world"
 
-hello_world
+hello_world="Solved this error!"
--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -1,3 +1,4 @@
 1 + 1
 
-1 + "is the loneliest number"
+1.to_s + "is the loneliest number"
+# I added ".to_s" to line 3 directly after the "1".
